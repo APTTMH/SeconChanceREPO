3cc34c32fd8dece91f3ab9c9937ab03b3a7de116
Dahime Gordon <Dahime@msn.com>
2023-02-03 10:42:15 -0500

    Changed plus to lasso

--- practice/ContentView.swift
+++ practice/ContentView.swift
@@ -36,7 +36,7 @@ struct ContentView: View {
                     }
                     ToolbarItem {
                         Button(action: addItem) {
-                            Label("Add Item", systemImage: "plus")
+                            Label("Add Item", systemImage: "lasso")
                         }
                     }
                 }
