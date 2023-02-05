56fa15441cb0ff6c0d3f6344a81e29b8ec3adbd1
kelly brown <kbrownb22@msu.idserve.net>
2023-02-05 16:07:44 -0500

    added DahimeView

---
+++ practice/DahimeView.swift
@@ -0,0 +1,20 @@
+//
+//  DahimeView.swift
+//  practice
+//
+//  Created by Kelly Brown on 2/5/23.
+//
+
+import SwiftUI
+
+struct DahimeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DahimeView_Previews: PreviewProvider {
+    static var previews: some View {
+        DahimeView()
+    }
+}
