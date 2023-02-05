3730a8dc0fec26b916712aa252841f6930acade4
Cameron Warner <cameron.w.warner@gmail.vom>
2023-02-05 16:06:52 -0500

    created template view

--- practice/MelanieTemplateView.swift
+++ practice/MelanieTemplateView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct MelanieTemplateView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Use Details Pro App to Create Me!")
     }
 }
 
