4460f5c438470cc431eb053e0754a4269aaa1849
Cameron Warner <cameron.w.warner@gmail.vom>
2023-02-08 11:39:19 -0500

    Updated Cam View with code from Details Pro.

--- practice/CamView.swift
+++ practice/CamView.swift
@@ -1,5 +1,5 @@
 //
-//  MyTrips.swift
+//  BlankiPhone.swift
 //  MyProject
 //
 //  Designed in DetailsPro
@@ -8,77 +8,55 @@
 
 import SwiftUI
 
-struct MyTrips: View {
+struct BlankiPhone: View {
     var body: some View {
-        ZStack {
+        VStack {
+            Spacer()
+            HStack {
+                Text("Scribble")
+                    .padding(.bottom, 0)
+                    .font(.largeTitle.weight(.bold))
+                    .fixedSize(horizontal: false, vertical: true)
+                Image(systemName: "scribble")
+                    .imageScale(.large)
+                    .symbolRenderingMode(.monochrome)
+                    .font(.largeTitle.weight(.black))
+                    .foregroundColor(.blue)
+                Text("Scraps!")
+                    .font(.largeTitle.weight(.bold))
+            }
             VStack {
                 HStack {
-                    Text("What Up Doe?!")
-                        .font(.headline)
-                        .foregroundColor(.secondary)
-                    Spacer()
-                    Text("Sign Out")
-                }
-                .padding(.top, 76)
-                HStack {
-                    Text("My Trips")
-                        .font(.title)
-                    Spacer()
-                }
-                .padding(.top, 36)
-                HStack(spacing: 10) {
                     Image("myImage")
                         .renderingMode(.original)
                         .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 70, height: 70)
+                        .aspectRatio(contentMode: .fit)
+                        .mask { RoundedRectangle(cornerRadius: 90, style: .continuous) }
+                        .frame(width: 200)
                         .clipped()
-                        .mask { RoundedRectangle(cornerRadius: 8, style: .continuous) }
-                    VStack(alignment: .leading, spacing: 2) {
-                        Text("Chicago, IL")
-                            .font(.system(size: 16, weight: .medium, design: .default))
-                        Text("May, 2023")
-                    }
-                    .font(.subheadline)
-                    Spacer()
-                    Image(systemName: "ellipsis")
-                        .foregroundColor(Color(.displayP3, red: 234/255, green: 76/255, blue: 97/255))
-                        .font(.title3)
-                }
-                .padding(.bottom, 4)
-                HStack(spacing: 10) {
                     Image("myImage")
                         .renderingMode(.original)
                         .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 70, height: 70)
+                        .aspectRatio(contentMode: .fit)
+                        .mask { RoundedRectangle(cornerRadius: 90, style: .continuous) }
+                        .frame(width: 200)
                         .clipped()
-                        .mask { RoundedRectangle(cornerRadius: 8, style: .continuous) }
-                    VStack(alignment: .leading, spacing: 2) {
-                        
-                    }
-                    .font(.subheadline)
-                    Spacer()
                 }
-                .padding(.bottom, 4)
-                HStack(spacing: 10) {
-                    VStack(alignment: .leading, spacing: 2) {
-                        
-                    }
-                    .font(.subheadline)
-                    Spacer()
-                }
-                .padding(.bottom, 4)
-                Spacer()
             }
-            .padding(.horizontal, 24)
+            Spacer()
+            Text("\"Scribble Long Scribble Wrong\"")
+                .font(.title3.weight(.bold))
+                .padding(30)
+            Spacer()
+            Divider()
+            Text("Coming Soon")
+            Spacer()
         }
-        .background(Image("myImage").resizable().aspectRatio(contentMode: .fill))
     }
 }
 
-struct MyTrips_Previews: PreviewProvider {
+struct BlankiPhone_Previews: PreviewProvider {
     static var previews: some View {
-        MyTrips()
+        BlankiPhone()
     }
 }
