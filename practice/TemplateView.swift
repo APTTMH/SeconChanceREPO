563d9aefc78080bcf06ba8a191dce9f1e8519b96
Cameron Warner <cameron.w.warner@gmail.vom>
2023-02-09 11:28:41 -0500

    added Background View && ShapeView

--- practice/TemplateView.swift
+++ practice/TemplateView.swift
@@ -9,33 +9,21 @@ import SwiftUI
 
 struct TemplateView: View {
     var body: some View {
-        NavigationView{
-            VStack{
-                HStack{
-                    Text("Title")
-                        .font(.title)
-//                        .frame(height:300)
-                        .padding(46)
-                }
-                Text("""
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Suspendisse non neque et nunc acilisis pretium in id urna.
-                    Nunc accumsan sapien id dapibus rutrum.
-                    """) //Crafting Stanzas for poem
-                .padding()
-                
-                
-//                Divider()
-                Image(systemName: "star")
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                
-            }//End of VStack
-            
+        NavigationStack {
+            ZStack{
+            BackgroundView()
+            RoundedBackgroundShapeView()
+                    .frame(height:700)
+                    .opacity(0.5)
+            }//End of ZStack
+          
         }// End of Nav. View
+        Image("secondChance2")
+            .resizable()
+        }
+        
     }
-}
+
 
 struct TemplateView_Previews: PreviewProvider {
     static var previews: some View {
