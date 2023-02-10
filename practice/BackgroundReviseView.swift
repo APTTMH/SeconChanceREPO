dff73a567648bd6562e589a03b29647694d5da5b
kelly brown <kbrownb22@msu.idserve.net>
2023-02-10 12:03:22 -0500

    home screen

--- practice/BackgroundReviseView.swift
+++ practice/BackgroundReviseView.swift
@@ -8,19 +8,43 @@
 import SwiftUI
 
 struct BackgroundReviseView: View {
-    var body: some View {
-        RoundedRectangle (cornerRadius: 25.0)
-            .fill(
-                Color.black 
-//                LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .top, endPoint: .bottom)
-            )
-//            .frame(width: 300, height: 300) //might use later
-            .ignoresSafeArea()
+    var body: some View{
+        NavigationStack{
+            ZStack{
+                
+                NavigationLink(""){
+                    WhatsNew()
+                }
+                    RoundedRectangle (cornerRadius: 25.0)
+                        .fill(
+                            Color.black
+                            //                LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .top, endPoint: .bottom)
+                        )
+                    //            .frame(width: 300, height: 300) //might use later
+                        .ignoresSafeArea()
+                    
+                    Image("secondChanceLogo")
+                    
+                    VStack{
+                        Spacer(minLength: 70)
+                        HStack{
+                            //Spacer()
+                            Text("To Reconnect | Develop")
+                                .foregroundColor(.orange)
+                                .font(.largeTitle)
+                                .padding(.bottom,75)
+                        }
+                    }
+                }// end of Z stack
+            }// end of Nav Stack
+            
+        }
+        
+        struct BackgroundReviseView_Previews: PreviewProvider {
+            static var previews: some View {
+                BackgroundReviseView()
+            }
+        }
     }
-}
+    
 
-struct BackgroundReviseView_Previews: PreviewProvider {
-    static var previews: some View {
-        BackgroundReviseView()
-    }
-}
