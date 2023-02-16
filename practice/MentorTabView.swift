e8dc4aa49eb3a3d9fa63350c672cee9855225bb7
Cameron Warner <cameron.w.warner@gmail.vom>
2023-02-16 09:15:14 -0500

    commented out MentorTabView b/c of trailing closure error.

--- practice/MentorTabView.swift
+++ practice/MentorTabView.swift
@@ -1,37 +1,39 @@
+////
+////  MentorTabView.swift
+////  practice
+////
+////  Created by Cameron Warner on 2/15/23.
+////
 //
-//  MentorTabView.swift
-//  practice
+//import SwiftUI
 //
-//  Created by Cameron Warner on 2/15/23.
+//struct MentorTabView: View {
+//    var body: some View {
+//    
+//            TabView {
+//                FaceTimeView()
+//                    .tabItem {
+//                        Image(systemName: "video")
+//                        Text("FaceTime")
+//                    }
+//                CalendarView()
+//                    .tabItem {
+//                        Image(systemName: "calendar")
+//                        Text("Calendar")
+//                    }
+//                MessageView()
+//                    .tabItem {
+//                        Image(systemName: "message")
+//                        Text("Message")
+//                    }
+//            }//End of TabView
+//            //        .font(.system(size:30, weight: .bold, design: .rounded))
+//        
+//        }
+//}
 //
-
-import SwiftUI
-
-struct MentorTabView: View {
-    var body: some View {
-        TabView{
-            FaceTimeView()
-            .tabItem {
-                Image(systemName: "video")
-                Text("FaceTime")
-            }
-            CalendarView()
-            .tabItem {
-                Image(systemName: "calendar")
-                Text("Calendar")
-            }
-           MessageView()
-                .tabItem {
-                Image(systemName: "message")
-                Text("Message")
-            }
-        }//End of TabView
-        .font(.system(size:30, weight: .bold, design: .rounded))
-    }
-}
-
-struct MentorTabView_Previews: PreviewProvider {
-    static var previews: some View {
-        MentorTabView()
-    }
-}
+//struct MentorTabView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MentorTabView()
+//    }
+//}
