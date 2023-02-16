c48c5016625d3b20d79842d53ccff3c894ea4a9a
kelly brown <kbrownb22@msu.idserve.net>
2023-02-16 11:06:49 -0500

    tab view

--- practice/BackgroundReviseView.swift
+++ practice/BackgroundReviseView.swift
@@ -14,17 +14,17 @@ struct BackgroundReviseView: View {
             ZStack{
                 
                 NavigationLink(""){
-                    WhatsNew() 
+                    WhatsNew()
                 }
-                    RoundedRectangle (cornerRadius: 25.0)
-                        .fill(
-                           
-                            //                LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .top, endPoint: .bottom)
-                        )
-                    //            .frame(width: 300, height: 300) //might use later
-                        .ignoresSafeArea()
-                    
-                    Image("secondChanceLogo")
+                RoundedRectangle (cornerRadius: 25.0)
+                    .fill(
+                        
+                        //                LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .top, endPoint: .bottom)
+                    )
+                //            .frame(width: 300, height: 300) //might use later
+                    .ignoresSafeArea()
+                
+                Image("secondChanceLogo")
                 RoundedRectangle (cornerRadius: 25.0)
                     .fill(
                         Color.black
@@ -35,32 +35,32 @@ struct BackgroundReviseView: View {
                 // NavigationLink("Tap me") {
                 //  Text("Destination")
                 Image("secondChanceLogo")
-
-                    
-                    VStack{
-                        Spacer(minLength: 70)
-                        HStack{
-        
-                            Text("To Reconnect | Develop")
-                                .foregroundColor(.orange)
-                                .font(.largeTitle)
-                                .padding(.bottom,75)
-                        }
-                        NavigationLink("TAP ME"){
+                
+                
+                VStack{
+                    Spacer(minLength: 70)
+                    HStack{
+                        
+                           NavigationLink("To Reconnect | Develop", destination: MentorTabView())
+                        .foregroundColor(.orange)
+                        .font(.largeTitle)
+                        .padding(.bottom,75)
+                    }
+                 //   NavigationLink("TAP ME"){
                         // end of Z stack
+                        
                     }// end of Nav Stack
                     
                 }
             }
         }
     }
-        struct BackgroundReviseView_Previews: PreviewProvider {
-            static var previews: some View {
-                BackgroundReviseView()
-            }
+    struct BackgroundReviseView_Previews: PreviewProvider {
+        static var previews: some View {
+            BackgroundReviseView()
         }
-        
-        
-        
     }
+    
+    
+    
 
